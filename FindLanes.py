1. Added test code to generate images for the writeup 2. Re-enabled and corrected the analytical computation of radius of curvature 3. Added writeup and the corresponding files

FindLanes.py
@@ -1,35 +1,40 @@
 import os
+import sys
 import cv2
 import glob
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from moviepy.editor import VideoFileClip
-from scipy.signal import find_peaks_cwt as findPeaks
-
+from collections import deque
+from itertools import chain
 
 # All the tunable settings in the program
 
 settings =  {
+            'diagnostic': False,
             'calibDims': (9, 6),
             'persrc': np.array([
-                   [703, 461],  # top right
+                   [703, 461], # top right
                    [1000,650], # bottom right
-                   [308, 650],  # bottom left
+                   [308, 650], # bottom left
                    [580, 461]  # top left
                             ], dtype=np.float32),
             'perdst': np.array([
                    [1000, 0  ], # top right
                    [1000, 700], # bottom right
-                   [300 , 700],  # bottom left
-                   [300 , 0  ]   # top left
+                   [300 , 700], # bottom left
+                   [300 , 0  ]  # top left
                              ], dtype=np.float32),
             'minPixelsForFit': 150,
             'minStdForFit': 100,
+            'maskSearchThickness':150,
             'lpf': 0.85,
-            'yScale': 30/270,
-            'xScale': 3.7/700,
-            'dropThresh': 5
+            'nFilt': 8,
+            'yScale': 30/800,  # m per pixel
+            'xScale': 3.7/700, # m per pixel
+            'dropThresh': 10
             }
 
 
@@ -129,19 +134,23 @@ class Line():
         #polynomial coefficients for the most recent fit
         self.currFit = [np.array([False])]
         #radius of curvature of the line in some units
-        self.radius_of_curvature = None
+        self.radius = None
+        self.vehPos = None
         #distance in meters of vehicle center from the line
         #self.line_base_pos = None
         #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float')
-        #x values for detected line pixels
+        #self.diffs = np.array([0,0,0], dtype='float')
+
+        # x values for detected line pixels in current frame
         self.pixx = None
-        #y values for detected line pixels
+        # y values for detected line pixels in current frame
         self.pixy = None
 
+        self.pastPixx = deque(maxlen=settings['nFilt'])
+        self.pastPixy = deque(maxlen=settings['nFilt'])
         self.yLine = np.arange(imgSize[0])
         self.xLine = None
-        self.xFilt = None
+        #self.xFilt = None
         self.mask = np.ones(imgSize, dtype=np.uint8)*255
 
         self.mode = 0
@@ -151,13 +160,23 @@ class Line():
     def fitLine(self, x, y):
         return np.polyfit(y, x, 2)
 
-    def curvature(self, x, y):
-        x = np.float64(x)*settings['xScale']
-        y = np.float64(y)*settings['yScale']
+    def curvature(self, x, y, yEval):
+        yEval = yEval*settings['yScale']
 
-        fitCoeffs = self.fitLine(x,y)
-        yEval = np.max(y)
-        return ((1 + (2*fitCoeffs[0]*yEval + fitCoeffs[1])**2)**1.5) / np.absolute(2*fitCoeffs[0])
+        ## Method 1: Fit again
+        #x = np.float64(x)*settings['xScale']
+        #y = np.float64(y)*settings['yScale']
+        #fitCoeffs = self.fitLine(x,y)
+        #return ((1 + (2*fitCoeffs[0]*yEval + fitCoeffs[1])**2)**1.5) / np.absolute(2*fitCoeffs[0])
+
+
+        ## Method 2: Derive analytical expression
+        fit = self.currFit
+        sx, sy = settings['xScale'], settings['yScale']
+        A = fit[0]*sx/sy/sy
+        B = fit[1]*sx/sy
+        return ((1 + (2*A*yEval + B)**2)**1.5) / np.absolute(2*A)
+
 
     def eval(self, y):
         return np.polyval(self.currFit, y)
@@ -167,21 +186,28 @@ class Line():
 
     def update(self,  x, y, mode):
         self.mode = mode
+
         if len(y) > settings['minPixelsForFit'] and np.std(y) > settings['minStdForFit']:
             self.pixx, self.pixy = x, y
-            self.currFit = np.polyfit(y, x, 2)
-            self.radius_of_curvature = self.curvature(x, y)
+            self.pastPixx.append(x)
+            self.pastPixy.append(y)
 
+
+            # Find the best fit line
+            fit = self.fitLine(list(chain.from_iterable(self.pastPixx)),
+                               list(chain.from_iterable(self.pastPixy)))
+            self.currFit = fit
+
+            # Remember the current line
             self.xLine = self.eval(self.yLine)
-            if not np.any(self.xFilt):
-                self.xFilt = self.xLine
 
-            lpf = settings['lpf']
-            self.xFilt = lpf*self.xFilt + (1-lpf)*self.xLine
+            self.radius = self.curvature(self.xLine, self.yLine, np.max(self.yLine))
+            self.vehPos = self.xLine[-1]
 
+            # Compute a search mask for the next frame
             self.mask.fill(0)
             linePts = np.transpose(np.vstack([self.xLine, self.yLine])).reshape((-1,1,2)).astype(np.int32)
-            cv2.drawContours(self.mask, linePts, -1, (255,255,255), thickness=100)
+            cv2.drawContours(self.mask, linePts, -1, (255,255,255), thickness=settings['maskSearchThickness'])
 
             self.detected = True
             self.dropCount = 0
@@ -266,21 +292,6 @@ def thresholdFrame(img):
 
 def computePerpectiveTransforms():
 
-    #srcPts = np.array([
-    #    [703, 461],  # top right
-    #    [1000,650], # bottom right
-    #    [308, 650],  # bottom left
-    #    [580, 461]  # top left
-    #], dtype=np.float32)
-
-    #dstPts = np.array([
-    #    [1000, 0  ], # top right
-    #    [1000, 700], # bottom right
-    #    [300 , 700],  # bottom left
-    #    [300 , 0  ]   # top left
-    #    ], dtype=np.float32)
-
-
     M    = cv2.getPerspectiveTransform(settings['persrc'], settings['perdst'])
     Minv = cv2.getPerspectiveTransform(settings['perdst'], settings['persrc'])
 
@@ -309,6 +320,15 @@ def changePerpective(img, M, visualize=False):
         plt.show()
 
     return changed
+
+def rejectOutliers(x, y, m = 2.):
+    # Since lines are mostly verticals, we reject outliers mostly in the xdirection
+    x, y = np.array(x), np.array(y)
+    d = np.abs(x - np.median(x))
+    mdev = np.median(d)
+    s = d/(mdev if mdev else 1.)
+    select = s<m
+    return list(x[select]), list(y[select])
 
 def calcBaseIdx(signal, threshold=10):
 
@@ -359,7 +379,6 @@ def slidingLanePixelSearch(img, nWin=10, margin=200, pixThres = 50, visualize=Fa
         yLeftCur , xLeftCur  = lWin.nonzero()
         yRightCur, xRightCur = rWin.nonzero()
 
-        # TODO: Reject outliers
 
         # Append to lane pixels
         yLeft.extend( yLeftCur  + winHeight*i)
@@ -367,12 +386,14 @@ def slidingLanePixelSearch(img, nWin=10, margin=200, pixThres = 50, visualize=Fa
         yRight.extend(yRightCur + winHeight*i)
         xRight.extend(xRightCur + idxStartRight - winWdith//2)
 
+        #xLeft, yLeft = rejectOutliers(xLeft, yLeft)
+        #xRight, yRight = rejectOutliers(xRight, yRight)
+
         if len(xLeftCur) > pixThres:
             idxStartLeft = np.int(np.median(xLeftCur)) + idxStartLeft - winWdith//2
         if len(xRightCur) > pixThres:
             idxStartRight = np.int(np.median(xRightCur)) + idxStartRight - winWdith//2
 
-    #cv2.putText(pltImg,'%.2f'%(np.std(yRight)),(50,50),  cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
 
     return yLeft, xLeft, yRight, xRight, pltImg
 
@@ -389,7 +410,6 @@ def processFrame(img):
     undistImg = cv2.undistort(img, state['mtx'], state['dist'], None, state['mtx'])
     perspImg = changePerpective(undistImg, state['per_m'])
     threshImg = thresholdFrame(perspImg).astype(np.uint8)
-
 
     if (leftLine.xLine is None) or (rightLine.xLine is None) or \
        (leftLine.dropCount > settings['dropThresh']) or (rightLine.dropCount > settings['dropThresh']):
@@ -408,8 +428,8 @@ def processFrame(img):
     diagImg = np.zeros((*threshImg.shape[:2], 3), dtype=np.uint8)
     #if detected:
     yLine = leftLine.yLine
-    xLineLeft  = leftLine.xFilt  #np.polyval(leftLineCoeffs, yLine)
-    xLineRight = rightLine.xFilt #np.polyval(rightLineCoeffs, yLine)
+    xLineLeft  = leftLine.xLine  #np.polyval(leftLineCoeffs, yLine)
+    xLineRight = rightLine.xLine #np.polyval(rightLineCoeffs, yLine)
 
     markImg = np.zeros((*threshImg.shape, 3), dtype=np.uint8)
 
@@ -425,22 +445,26 @@ def processFrame(img):
     markImg = cv2.warpPerspective(markImg, state['per_minv'], (img.shape[1], img.shape[0]))
     # Combine the result with the original image
     result = cv2.addWeighted(undistImg, 1, markImg, 0.3, 0)
+    # Add text annotations
+    error = (img.shape[1] - (rightLine.vehPos + leftLine.vehPos))*settings['xScale']/2
+    cv2.putText(result,
+            'Mean radius of curvature: %d m'%( np.int(np.mean([leftLine.radius, rightLine.radius]))), (50,50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    cv2.putText(result,
+            'Cross-track error: %.3f m'%(error), (50,100),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
 
     ## Create a lane detection diagnostic image
     diagImg[leftLine.pixy, leftLine.pixx] = [255,0,0]
     diagImg[rightLine.pixy, rightLine.pixx]=[0,0,255]
 
     # Change the annotation based on mode
-
     diagImg[:,:,1] = winOver
     cv2.polylines(diagImg, np.int32([ptsLeft]), isClosed=False, color=[255, 255, 0], thickness=8)
     cv2.polylines(diagImg, np.int32([ptsRight]), isClosed=False, color=[255, 255, 0], thickness=8)
-    #else:
-    #    result = undistImg
 
-    diagnostic = True
-    if diagnostic:
 
+    if settings['diagnostic']:
         imgTopLeft  = undistImg
         imgTopRight = np.dstack((threshImg,threshImg,threshImg))
         imgBotLeft  = diagImg
@@ -456,26 +480,24 @@ def processFrame(img):
 
 
 if __name__=='__main__':
-    calDataFile = 'calib_data.npz'
+
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+
+    calDataFile = 'calibData.p'
     calImageFiles = 'camera_cal/calibration*.jpg'
-
-    #dist_pickle = {}
-    #dist_pickle['mtx'] = mtx
-    #dist_pickle['dist'] = dist
-    #pickle.dump(dist_pickle, open('./camera_cal/calibration_pickle.p', 'wb'))
-
-    #dist_pickle = pickle.load(open('./camera_cal/calibration_pickle.p', 'rb'))
-    #mtx = dist_pickle['mtx']
-    #dist = dist_pickle['dist']
 
     if os.path.isfile(calDataFile):
         print('Loading camera calibration ...')
-        loaded = np.load(calDataFile)
+        loaded = pickle.load(open(calDataFile, 'rb'))
         mtx, dist = loaded['mtx'], loaded['dist']
     else:
         print('Computing camera calibration ...')
         mtx, dist = setupAndCalib(calImageFiles)
-        np.savez(calDataFile, mtx=mtx, dist=dist)
+        toPickle = {}
+        toPickle['mtx'] = mtx
+        toPickle['dist'] = dist
+        pickle.dump(toPickle, open(calDataFile, 'wb'))
 
 
     state = {'mtx': mtx,
@@ -486,6 +508,64 @@ if __name__=='__main__':
 
 
     state['per_m'], state['per_minv'] = computePerpectiveTransforms()
+
+    #videoIn = VideoFileClip(inFile).subclip(20, 26)
+    #videoIn = VideoFileClip(inFile).subclip(37, 42)
+    videoIn = VideoFileClip(inFile)
+    videoOut = videoIn.fl_image(processFrame)
+    videoOut.write_videofile(outFile, audio=False)
+
+    ## Test thresholding
+    #inFile = 'test_images\straight_lines1.jpg'
+    #inImg = mpimg.imread(inFile)
+    #outImg = processFrame(inImg)
+    #f, (ax1, ax2) = plt.subplots(1, 2)
+    #ax1.set_xticks([])
+    #ax1.set_yticks([])
+    #ax2.set_xticks([])
+    #ax2.set_yticks([])
+    #ax1.imshow(inImg)
+    #ax1.set_title('Input colour')
+    #ax2.imshow(outImg, cmap='gray')
+    #ax2.set_title('Thresholded binary')
+    #plt.savefig('output_images/threshresult.png', bbox_inches='tight')
+    #plt.show()
+
+    ## Show perspective correction
+    #inFile = 'test_images\straight_lines1.jpg'
+    #inImg = mpimg.imread(inFile)
+    #outImg = processFrame(inImg)
+    #f, (ax1, ax2) = plt.subplots(1, 2)
+    #ax1.set_xticks([])
+    #ax1.set_yticks([])
+    #ax2.set_xticks([])
+    #ax2.set_yticks([])
+    #ax1.imshow(inImg)
+    #ax1.set_title('Camera')
+    #x, y = np.hsplit(settings['persrc'], 2)
+    #ax1.plot(x, y, 'r', linewidth=3)
+    #ax2.imshow(outImg)
+    #ax2.set_title('Corrected')
+    #x, y = np.hsplit(settings['perdst'], 2)
+    #ax2.plot(x, y, 'r', linewidth=3)
+    #plt.savefig('output_images/perspresult.png', bbox_inches='tight')
+    #plt.show()
+
+    ## Test camera calibration
+    #inFile = 'camera_cal\calibration1.jpg'
+    #inImg = mpimg.imread(inFile)
+    #outImg = cv2.undistort(inImg, state['mtx'], state['dist'], None, state['mtx'])
+    #f, (ax1, ax2) = plt.subplots(1, 2)
+    #ax1.set_xticks([])
+    #ax1.set_yticks([])
+    #ax2.set_xticks([])
+    #ax2.set_yticks([])
+    #ax1.imshow(inImg)
+    #ax1.set_title('Uncalibrated')
+    #ax2.imshow(outImg)
+    #ax2.set_title('Calibrated')
+    #plt.savefig('output_images/calibresult.png', bbox_inches='tight')
+    #plt.show()
 
     #imgStack, _ = readFolderToStack()
     #outStack = [processFrame(img) for img in imgStack]
@@ -499,17 +579,11 @@ if __name__=='__main__':
     #outImg = processFrame(inImg)
 
     #fig = plt.figure()
-    ##plt.subplot(1, 2, 1)
-    #plt.imshow(outImg)
+    #plt.subplot(1, 2, 1)
+    #plt.imshow(inImg)
 
     ##plt.subplot(1, 2, 2)
     ##plt.imshow(outImg, cmap='gray' )
     #plt.show()
 
-    inFile = 'project_video.mp4'
-    outFile = 'project_video_out.mp4'
-    #videoIn = VideoFileClip(inFile).subclip(20, 26)
-    videoIn = VideoFileClip(inFile)
-    videoOut = videoIn.fl_image(processFrame)
-    videoOut.write_videofile(outFile, audio=False)
 